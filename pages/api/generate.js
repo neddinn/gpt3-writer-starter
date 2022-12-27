3c738b6e99e7b35bc40b71a75a55fe8a19cdd60e
neddinn <neddinn@gmail.com>
2022-12-27 00:57:05 +0000

    Initial commit

---
+++ pages/api/generate.js
@@ -0,0 +1,57 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// const basePromptPrefix = `
+//   Give me a very detailed explanation of the following concept,
+//   explaining it in clear terms and making it understandable by 5 year olds,
+//   and also giving examples where applicable, and ending with a summary
+// `;
+
+const basePromptPrefix = `
+  List at most 4 important topics that should be covered when explaining the following
+  concept in simple and clear terms:
+
+  Concept:
+`;
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  if (req.method != 'POST') return res.status(404).json({ error: 'Route not found' });
+
+  const userInput = req.body.userInput;
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${userInput}\n`,
+    temperature: 0.4,
+    max_tokens: 250,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  const secondPrompt = `
+    Using the following list of topics as guidelines, Give me a detailed explanation of ${userInput},
+    explaining it in clear terms and making it understandable to 5 year olds without including the topics in the explanation.
+
+    Topics: ${basePromptOutput.text}
+
+    Explanation:
+  `;
+
+    const secondCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${secondPrompt}\n`,
+      temperature: 0.4,
+      max_tokens: 450,
+    });
+
+  const secondPromptOutput = secondCompletion.data.choices.pop();
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,100 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [prompt, setPrompt] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [apiOutput, setApiOutput] = useState('');
+
+  const handleGenerate = async () => {
+    if (!prompt) return;
+    setIsGenerating(true);
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput: prompt }),
+    });
+    setIsGenerating(false);
+    const data = await response.json();
+    const { output } = data;
+    setApiOutput(`${output.text}`);
+  };
+
   return (
-    <div className="root">
+    <div className='root'>
       <Head>
         <title>GPT-3 Writer | buildspace</title>
       </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+      <div className='container'>
+        <div className='header'>
+          <div className='header-title'>
+            <h1>ELI5 (Explain like I'm 5)</h1>
           </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+          <div className='header-subtitle'>
+            <h2>
+              Write a concept you want explained to you in the simplest terms
+            </h2>
           </div>
         </div>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+      <div className='prompt-container'>
+        <textarea
+          className='prompt-box'
+          placeholder='start typing here..'
+          onChange={(e) => setPrompt(e.target.value)}
+          value={prompt}
+        ></textarea>
+        <div className='prompt-buttons'>
+          <a className='generate-button' onClick={handleGenerate}>
+            <div className='generate'>
+              {isGenerating ? (
+                <span className='loader'></span>
+              ) : (
+                <p>Generate</p>
+              )}
+            </div>
+          </a>
+        </div>
+        {apiOutput && (
+          <div className='output'>
+            <div className='output-header-container'>
+              <div className='output-header'>
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className='output-content'>
+              <p>{apiOutput}</p>
+            </div>
           </div>
-        </a>
+        )}
+      </div>
+      <div>
+        <div className='badge-container grow'>
+          <a
+            href='https://buildspace.so/builds/ai-writer'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <div className='badge'>
+              <Image src={buildspaceLogo} alt='buildspace logo' />
+              <p>build with buildspace</p>
+            </div>
+          </a>
+          <div className='download'>
+            Like this tool?{' '}
+            <a
+              href='https://github.com/neddinn/eli5-chrome-extension/archive/refs/heads/main.zip'
+              download
+            >
+              {' '}
+              Download the Extension
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
